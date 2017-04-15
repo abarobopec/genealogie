24a9d3c0ccae5870552b6b64e101295adcf6d792
Adrien BARRAL <aba@robopec.com>
2017-04-15 16:20:42 +0200

    Modif pour la nouvelle beta de Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {MaterialModule, MdMenuModule, MdButtonModule, MdDialogModule} from '@angular/material';
+import { MdToolbarModule, MdMenuModule, MdButtonModule, MdDialogModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ZoomableImageComponent } from './zoomable-image/zoomable-image.component';
 import { DownloadDialogComponent } from './download-dialog/download-dialog.component';
 import { ExplainDialogComponent } from './explain-dialog/explain-dialog.component';
+import { NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -20,9 +21,10 @@ import { ExplainDialogComponent } from './explain-dialog/explain-dialog.componen
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
+    NoopAnimationsModule,
     MdMenuModule,
     MdButtonModule,
+    MdToolbarModule,
     MdDialogModule
   ],
   providers: [],
